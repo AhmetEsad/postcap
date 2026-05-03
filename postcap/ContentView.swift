5211800211a4e4a1d4dc8bdc383a0f8086ce4d74
Ahmet Esad <38183175+AhmetEsad@users.noreply.github.com>
2026-05-03 18:44:59 +0300

    Initial Commit

---
+++ postcap/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  postcap
+//
+//  Created by ahmet on 03/05/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ postcap/postcapApp.swift
@@ -0,0 +1,17 @@
+//
+//  postcapApp.swift
+//  postcap
+//
+//  Created by ahmet on 03/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct postcapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
